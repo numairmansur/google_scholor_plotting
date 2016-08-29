3f338df1b69b6f986a54fda439ebbbeb599beebb
numairmansur <numair.mansur@gmail.com>
2016-08-29 11:34:56 +0200

    changes

--- plotting.py
+++ plotting.py
@@ -11,6 +11,7 @@ import csv
 import sys
 import os
 import argparse
+import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 
 def csv_column_reader(filepath):
@@ -55,6 +56,10 @@ def plotter(dictionary,csv_columns, pdf_file_name, color_flag, order,y_max,y_inc
 	'''
 	Produces a plot
 	'''
+	sns.set(style="white", color_codes=True, font_scale=1.2)
+	sns.set_style("ticks", {"xtick.major.size": 3, "ytick.major.size": 2})
+
+
 	default = 0
 	years = []
 	papers = {}
@@ -113,8 +118,6 @@ def plotter(dictionary,csv_columns, pdf_file_name, color_flag, order,y_max,y_inc
 	plt.yticks(np.arange(0, max(total_number_of_papers_in_a_year) + max(total_number_of_papers_in_a_year)/5 if y_max == None else int(y_max)+1,
 		int(y_increment)),
 		fontsize=19)
-	
-	y_max
 
 	#Reverse the legend order
 	bars= list(reversed(bars))
@@ -125,9 +128,62 @@ def plotter(dictionary,csv_columns, pdf_file_name, color_flag, order,y_max,y_inc
 	pp = PdfPages(pdf_file_name)
 	plt.savefig(pp, format='pdf')
 	pp.close()
-	plt.show()
+	return plt
 
+def bar_plot(x, curves, title="", width=0.15,
+            colors=['b', 'g', 'r', 'c', 'm', 'y', 'k'], legend=True,
+            x_title="Year", y_title="# citations",bar_names = [],color="yes", pdf_file_name="default"):
+    # Set Appearance properties using Seaborn
+    sns.set(style="white", color_codes=True, font_scale=1.2)
+    sns.set_style("ticks", {"xtick.major.size": 3, "ytick.major.size": 2})
+    #sns.set_titles(col_template = "{col_name}", fontweight = 'bold', size = 18)
+    # - - - - - - - -- - - - - - - - -
+    if color == "yes":
+    	colors=['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    else:
+    	colors = ['#d9d8d8', '#b3b2b2', '#7b7979', '#555353', '#434040', '#2e2c2c', '#1a1919', '#0d0c0c']
+    x = x.astype(float)
+    fig, ax = plt.subplots()
+    bars = []
+    # Stores the information about how many bars are saved on a X-Location.
+    x_location_map = dict()
 
+    # Pre-Process and adjust X-axis location data
+    for i, j in enumerate(x):
+        for l, k in enumerate(j):
+            if k in x_location_map:
+                x[i][l] = x[i][l] + (x_location_map[k] * width)
+                x_location_map[k] += 1
+            else:
+                x_location_map[k] = 1
+    # - - - - - - - - - - - - -
+    for i, j in enumerate(x):
+        bar = ax.bar(
+            j,
+            curves[i][0],
+            width,
+            yerr=curves[i][1],
+            color=colors[i],
+            error_kw=dict(
+                ecolor='#525252',
+                capsize=0,
+                capthick=0))
+        bars.append(bar)
+
+    bars = list(reversed(bars))
+    bar_names = list(reversed(bar_names))
+    ax.set_ylabel(y_title, fontsize=14)
+    ax.set_xlabel(x_title, fontsize=14)
+    ax.set_title(title, fontsize=15)
+    ax.set_xticks([i + ((x_location_map[i] * width) / 2)
+                   for i in x_location_map])
+    ax.set_xticklabels([int(i) for i in x_location_map])
+    if legend:
+    	ax.legend(bars, bar_names, loc=0)
+	pp = PdfPages(pdf_file_name)
+    plt.savefig(pp, format='pdf')
+    pp.close()
+    return plt
 
 
 def main():
@@ -156,14 +212,34 @@ def main():
 	for i in dicti:
 		items_in_csv.append(i)
 	if set(order) == set(items_in_csv) and len(items_in_csv) == len(order):
-		plotter(dicti,csv_columns, pdf, color, order,y_max,y_increment)
+		plot =plotter(dicti,csv_columns, pdf, color, order,y_max,y_increment)
 	elif len(order) == 0:
-		plotter(dicti,csv_columns, pdf, color, items_in_csv,y_max,y_increment)
+		plot =plotter(dicti,csv_columns, pdf, color, items_in_csv,y_max,y_increment)
 	else:
 		print "\n <ERROR> Either length of the order arguments is not 3 or they are not in the csv file"
 		print "Please select the order from among the items in following list:"
 		print items_in_csv
 		print "\n"
+
+	#The other curve.
+	x=[]
+	curves = []
+	bar_names=[]
+	for i in dicti:
+		bar_names.append(i)
+		x_axis = [int(j) for j in dicti[i]]
+		x_axis.sort()
+		curve = []
+		for j in x_axis:
+			curve.append(dicti[i][str(j)])
+		curves.append(np.array([curve,[0 for i in curve]]))
+		x.append(x_axis)
+
+	x=np.array(x,dtype=object)
+	plot = bar_plot(x, curves, legend = True,bar_names=bar_names,color=color,pdf_file_name = pdf+"2")
+	plot.show()
+
+
 
 if __name__ == "__main__":
 	main()
